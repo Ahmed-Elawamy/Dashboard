b3804679c3907689c4ddf7d17cceb85caab422b9
Elawamy <ahmedalawamy10px@gmail.com>
2025-12-12 00:18:45 +0200

    update: fixed Sidebar import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Sidebar from "./pages/Sidebar.jsx";
+import Sidebar from "./Pages/Sidebar.jsx";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Copmonent/Dashboard";
 import Students from "./Copmonent/Students";
